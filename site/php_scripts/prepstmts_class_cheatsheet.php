2bca9a8fc965ec83c53bc5e88fe2f17e88fd956c
Jan Lipnican <concreterdesign@gmail.com>
2014-12-19 21:35:56 +0100

    basic structure

---
+++ site/php_scripts/prepstmts_class_cheatsheet.php
@@ -0,0 +1,13 @@
+<?php
+
+// some cheat sheet 
+
+include 'include/prepared_statements.php';
+
+/*$db = new DB('root', '', 'test');
+print_r($db->select('SELECT * FROM objects WHERE ID = ?', array(10), array('%d')));*/
+
+//                                                              ^          ^ 
+//                                                          variables   data-type ... remember to put as much %d(integer or %s as string) as much variables are there 
+
+?>
